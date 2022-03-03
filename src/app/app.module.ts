7a170ab2fe13ecf519b8f4023a7fa0937745df81
Rafael Perini de Souza <rafael.perini@uniodigital.com.br>
2022-03-03 15:52:40 -0300

    chore: fix angular base href.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ListComponent } from './views/list/list.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/pokedex'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
