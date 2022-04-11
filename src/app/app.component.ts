a26d88c8370a80bb3e8bfda879577f62c159e05c
Rafael Perini <rafael_perini_1997@hotmail.com>
2022-04-11 14:39:55 -0300

    feat: add analytics call on app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { Router, NavigationEnd } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'pokedex';
+
+  constructor(private router: Router) {
+    this.router = router;
+    this.trackPageView();
+  }
 
   ngOnInit(): void {
       window.onscroll = () => {
@@ -15,5 +24,13 @@ export class AppComponent implements OnInit{
 
         if (pokeballBack) pokeballBack.style.transform = rotation;
       }
+  }
+
+  private trackPageView(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(({ urlAfterRedirects }: NavigationEnd) => {
+        gtag('event', 'page_view', { page_path: urlAfterRedirects });
+      });
   }
 }
